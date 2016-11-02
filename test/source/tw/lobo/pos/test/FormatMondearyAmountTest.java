81127d5f7a5c0ba89f82e33af8cd9f697abca7e7
Lobo Chu <Lobo.Chu@audatex.com>
2016-11-02 12:01:31 +0800

    Replaced single test with parameterised test

--- test/source/tw/lobo/pos/test/FormatMondearyAmountTest.java
+++ test/source/tw/lobo/pos/test/FormatMondearyAmountTest.java
@@ -2,6 +2,11 @@ package tw.lobo.pos.test;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Collection;
+import java.util.Collections;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
@@ -9,14 +14,33 @@ import static org.junit.Assert.assertEquals;
 /**
  * Created by twlcbo on 2016/11/2.
  */
+@RunWith(Parameterized.class)
 public class FormatMondearyAmountTest {
+
+    private final int priceInCents;
+    private final String expectedFormattedPrice;
+
+    public FormatMondearyAmountTest(int priceInCents, String expectedFormattedPrice) {
+        this.priceInCents = priceInCents;
+        this.expectedFormattedPrice = expectedFormattedPrice;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+
+        return Collections.singletonList(
+                new Object[]
+                        {789, "$7.89"}
+        );
+    }
+
     private static String format(int priceInCents) {
         return "$7.89";
     }
 
     @Test
-    public void simplest() throws Exception {
-        assertEquals("$7.89", format(789));
+    public void test() throws Exception {
+        assertEquals(expectedFormattedPrice, format(priceInCents));
 
     }
 
