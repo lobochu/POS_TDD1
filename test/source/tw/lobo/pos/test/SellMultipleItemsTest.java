d0df8ae50e87f5231795a91abc05c9a7b9e90f1e
Lobo Chu <Lobo.Chu@audatex.com>
2016-11-22 11:05:35 +0800

    We can handle scanning multiple items without finding their prices

--- test/source/tw/lobo/pos/test/SellMultipleItemsTest.java
+++ test/source/tw/lobo/pos/test/SellMultipleItemsTest.java
@@ -55,6 +55,31 @@ public class SellMultipleItemsTest {
     }
 
     @Test
+    public void severalItemsAllNotFound() throws Exception {
+        Display display = new Display();
+        Sale sale = new Sale(
+                catalogWithoutBarcodes("product you won't find",
+                        "another product your wont' find",
+                        "a third product you won't find")
+                , display);
+
+        sale.onBarcode("product you won't find");
+        sale.onBarcode("another product your wont' find");
+        sale.onBarcode("a third product you won't find");
+        sale.onTotal();
+
+        assertEquals("No sale in progress. Try scanning a product.", display.getText());
+    }
+
+    private Catalog catalogWithoutBarcodes(String... barcodesToExclude) {
+        return emptyCatalog();
+    }
+
+    private Catalog emptyCatalog() {
+        return new Catalog(Collections.emptyMap());
+    }
+
+    @Test
     public void zeroItems() throws Exception {
         Display display = new Display();
 
